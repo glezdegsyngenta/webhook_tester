225a4d1c19d3a2e0fbfa0246ab95943d6bea5c27
Diego Gonzalez Gonzalez <diego.gonzalez-1@syngenta.com>
2022-08-31 16:20:14 -0500

    Initial setup to test webhooks

---
+++ webhook_tester_app.py
@@ -0,0 +1,14 @@
+from crypt import methods
+from urllib import request
+from flask import Flask,request
+
+app = Flask(__name__)
+
+@app.route("/", methods=['POST'])
+def hello_world():
+    if request.method=='POST':
+        print(request.json)
+        return request.json,200
+
+if __name__ == "__main__":
+    app.run(debug=True)
